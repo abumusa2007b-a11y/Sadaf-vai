0f60e0ed363894b070a5d157974a30442b26a3cb
abumusa2007b-a11y <abumusa2007b@gmail.com>
2026-05-20 17:48:07 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import time
+import random
+import os
+
+TOKEN = os.getenv("8647508858:AAHvcKhgzmFL2lwShGZzE3S_Rg-sqt9TqZA") or "YOUR_TOKEN"
+CHAT_ID = os.getenv("-1003993079945") or "YOUR_CHAT_ID"
+
+sizes = ["BIG", "SMALL"]
+colors = ["RED", "GREEN", "VIOLET"]
+numbers = [str(i) for i in range(10)]
+
+def generate_signal():
+    size = random.choice(sizes)
+    color = random.choice(colors)
+    number = random.choice(numbers)
+
+    message = f"""ARIYAN BHAI!! 👋
+SIZE : {size} 📏
+COLOUR : {color} 🎨
+NUMBER : {number} 🔢
+"""
+    return message
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": text}
+    requests.post(url, data=data)
+
+while True:
+    send_message(generate_signal())
+    time.sleep(30)  # 30 seconds delay
